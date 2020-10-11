e7144c1cb28e6fb01bf9030cf1cd4c1dff14298c
israel <israel.usp@gmail.com>
2020-10-11 17:51:22 -0300

    - Criadas pastas para o backend (brain), para o banco de dados (brain/DAOs) e para modelos (modelos); - Conexões entre as telas criadas; - Funções para criação do banco de dados do usuário e busca do banco com nome definido criadas;

---
+++ brain/brainCadastro.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import pyqtSignal
+from Telas.cadastro import Ui_mwCadastro
+from modelos.usuario import Usuario
+
+
+class brainCadastro(Ui_mwCadastro, QMainWindow):
+    home = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(Ui_mwCadastro, self).__init__(parent)
+        self.setupUi(self)
+        self.pbVoltarLogin.clicked.connect(self.goHome)
+        self.home.connect(self.parent().backHome)
+
+        self.usuario = Usuario()
+
+        self.leNomeEmpresa.textChanged.connect(self.exibe)
+
+    def goHome(self):
+        self.home.emit()
+
+    def exibe(self, texto):
+        print(texto)
