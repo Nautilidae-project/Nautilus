f08552b39509aa4c78e38281001c30cdbe2d7134
RenanSoares <renan.silva.soares.br@gmail.com>
2020-10-18 16:20:07 -0300

    Banco de Dados -criaBancoEstados e addEstados- adicionados

--- brain/DAOs/brainUserConfig.py
+++ brain/DAOs/brainUserConfig.py
@@ -22,6 +22,54 @@ def criaBanco(nomeBanco):
 
     cursor.executescript(strComando)
 
+def criaBancoEstados():
+    conn = sqlite3.connect(join(dirname(__file__), 'estados.db'))
+    cursor = conn.cursor()
+
+    strComando = """CREATE TABLE IF NOT EXISTS estados(
+                    estados VARCHAR(20) NOT NULL);"""
+
+    cursor.executescript(strComando)
+
+
+def addEstados():
+    conn = sqlite3.connect(join(dirname(__file__), 'estados.db'))
+    cursor = conn.cursor()
+
+    listaEstados = {
+        'Acre',
+        'Alagoas',
+        'Amapá',
+        'Amazonas',
+        'Bahia',
+        'Ceará',
+        'Distrito Federal',
+        'Espírito Santo',
+        'Goiás',
+        'Maranhão',
+        'Mato Grosso',
+        'Mato Grosso do Sul',
+        'Minas Gerais',
+        'Pará',
+        'Paraíba',
+        'Paraná',
+        'Pernambuco',
+        'Piauí',
+        'Rio de Janeiro',
+        'Rio Grande do Norte',
+        'Rio Grande do Sul',
+        'Rondônia',
+        'Roraima',
+        'Santa Catarina',
+        'São Paulo',
+        'Sergipe',
+        'Tocantins'
+    }
+
+    for estado in listaEstados:
+        strComando = f"""INSERT INTO estados (estados) VALUES ('{estado}')"""
+        cursor.executescript(strComando)
+
 
 def buscaBanco(nomeBanco):
     listCurrentDir = listdir(dirname(__file__))
@@ -32,3 +80,6 @@ def buscaBanco(nomeBanco):
     print(f'Nenhum banco de dados com o nome {nomeBanco} listado')
     return False
 
+# criaBanco('Renan')
+# criaBancoEstados()
+# addEstados()
