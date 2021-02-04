30e8d6aa7ef65e3bdb8441817b6d7c9d7f17f458
Renan <renan.silva.soares.br@gmail.com>
2021-02-04 17:44:17 -0300

    Implementação  dos cards  de Eventos  -não finalizados

--- brain/dashboard/Agenda/agendaPage.py
+++ brain/dashboard/Agenda/agendaPage.py
@@ -1,19 +1,23 @@
 from datetime import datetime
+from math import ceil
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate, Qt, QSize
 from PyQt5.QtGui import QMouseEvent
 
 from Telas.dashAgenda import Ui_wdgAgenda
 
-from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5.QtWidgets import QWidget, QGridLayout, QVBoxLayout, QLabel
 
 from brain.DAOs.daoGrupo import DaoGrupo
 from brain.dashboard.Agenda.agendaController import CalendarioController
 from brain.DAOs.daoEvento import DaoEvento
+from brain.dashboard.Agenda.localWidgets.eventosCard import EventosCard
 from brain.dashboard.Cliente.localWidgets.gruposCard import GruposCard
 from modelos import eventoModel
 from modelos.eventoModel import EventoModelo
+from modelos.grupoModel import GrupoModelo
+from modelos.efeitosModel import Efeitos
 
 
 class AgendaPage(Ui_wdgAgenda, QWidget):
@@ -29,7 +33,12 @@ class AgendaPage(Ui_wdgAgenda, QWidget):
 
         self.evento = EventoModelo()
 
+        self.gridBox = QGridLayout()
+        self.efeito = Efeitos()
 
+        self.atualizaGruposCards()
+
+        self.colunas = 0
 
         #Dao's
         self.daoEvento = DaoEvento()
@@ -44,12 +53,96 @@ class AgendaPage(Ui_wdgAgenda, QWidget):
         # self.calendario.mouseDoubleClickEvent.connect(self.addData)
 
         # Criando um Evento
-        self.pbAddEvento.clicked.connect(lambda : self.criaEvento())
+        self.pbAddEvento.clicked.connect(self.criaEvento)
 
         # Adiciona os Grupos na ComboBox
         self.cbxGrupos.addItems(self.dictDosGruposFormados().keys())
 
-        print(self.daoEvento.buscaDatasEventos())
+        self.calendario.clicked[QDate].connect(self.printDataSelecionada)
+
+    def atualizaGruposCards(self):
+
+        
+
+        colunas = 1
+
+        # Busca no banco de dados todos os grupos criados
+
+        eventosCadastrados = DaoEvento().findAll()
+
+        widthCard = self.saEventosCard.sizeHint().width() + 20
+        widthScreen = self.window().size().width()
+
+        if len(eventosCadastrados) > 8 and 3 * widthCard < widthScreen:
+            colunas = 2
+        elif len(eventosCadastrados) > 3 and 2 * widthCard < widthScreen:
+            colunas = 1
+
+
+        self.colunas = colunas
+
+        linhas = ceil(len(eventosCadastrados) / colunas)
+        print(linhas, ' ------')
+
+        # Cria um lista contendo o modelo de cada grupo buscado no banco de dados
+
+        eventoModelos = [EventoModelo(eventosCadastrados[i]) for i in range(0, len(eventosCadastrados))]
+
+        # Cria uma lista contendo os layouts dos cards com as informações dos modelos contidos no eventoModelos
+        listaEventosCards = [EventosCard(parent=self, evento=eventoModelos[i]) for i in range(0, len(eventosCadastrados))]
+        print(len(listaEventosCards))
+
+        # Cria uma matriz das posições nas quais os cards serão apresentados
+        posicoes = [(linha, coluna) for linha in range(linhas) for coluna in range(colunas)]
+
+        for card, posicao in zip(listaEventosCards, posicoes):
+            self.efeito.shadowCards([card], color=(105, 210, 231, 80))
+            self.gridBox.addWidget(card, *posicao)
+        # self.gridBox.setSpacing(32)
+
+        if self.gridBox.count():
+            self.saEventosCard.setLayout(self.gridBox)
+
+
+    def printDataSelecionada(self):
+        for data in DaoEvento().buscaDatasEventos().values():
+            if self.calendario.selectedDate() == data:
+                print(data, ' ---')
+
+    def redimensionaTela(self):
+        sizeCard = EventosCard().sizeHint().width() + 20
+        sizeArea = self.saEventosCard.size().width()
+
+        if 3*sizeCard < sizeArea:
+            colunas = 3
+        elif 2*sizeCard < sizeArea:
+            colunas = 2
+        else:
+            colunas = 1
+
+        if colunas == self.colunas:
+            return True
+        else:
+            self.colunas = colunas
+
+            listaCards = list()
+
+            for i in range(self.gridBox.count()):
+                listaCards.append(self.gridBox.itemAt(i).widget())
+
+            self.limpaLayout()
+
+            linhas = ceil(len(listaCards) / colunas)
+
+            posicoes = [(linha, coluna) for linha in range(linhas) for coluna in range(colunas)]
+
+            for card, posicao in zip(listaCards, posicoes):
+                self.efeito.shadowCards([card], color=(105, 210, 231, 80))
+                self.gridBox.addWidget(card, *posicao)
+            self.gridBox.setSpacing(32)
+
+            if self.gridBox.count():
+                self.scrollGrupos.setLayout(self.gridBox)
 
     def criaEvento(self):
 
@@ -62,11 +155,11 @@ class AgendaPage(Ui_wdgAgenda, QWidget):
         self.evento.titulo = self.leTituloEvento.text()
         self.evento.detalhe = self.teDetalhesEvento.toPlainText()
         self.evento.grupoId = self.dictDosGruposFormados()[self.cbxGrupos.currentText()]
-        # self.evento.dataEvento = datas[0]
-        self.evento.dataEvento = self.deDataEvento
-        a = self.deDataEvento.dateTime().toPyDateTime()
-        print(a)
-        print(QDate)
+        self.evento.dataEvento = datas[0]
+        # self.evento.dataEvento = self.deDataEvento
+        # a = self.deDataEvento.dateTime().toPyDateTime()
+        # print(a)
+        # print(QDate)
         self.evento.dataCadastro = None
         self.evento.horaInicio = datas[1]
         self.evento.horaFim = datas[2]
